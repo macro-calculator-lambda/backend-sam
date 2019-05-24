181782e20a49b7e25a12ae404b28346696ca3959
arodri04 <47488166+arodri04@users.noreply.github.com>
2019-05-24 16:11:08 -0600

    Cafr commit

--- java-healththingy/src/main/java/com/lambdaschool/health/config/ResourceServerConfig.java
+++ java-healththingy/src/main/java/com/lambdaschool/health/config/ResourceServerConfig.java
@@ -42,7 +42,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter
             .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
 
 //         http.requiresChannel().anyRequest().requiresSecure();
-        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll();
+        http.csrf().disable();
         http.headers().frameOptions().disable();
     }
 }
